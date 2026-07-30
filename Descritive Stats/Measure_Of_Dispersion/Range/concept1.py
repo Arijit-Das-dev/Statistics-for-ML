bd7addf1912cf6db071e3098adc67994cbf99c49
Arijit Das <arijitdas.das99@gmail.com>
2026-07-30 15:04:32 +0530

    Measure of Dispersion: Range

---
+++ Descritive Stats/Measure_Of_Dispersion/Range/concept1.py
@@ -0,0 +1,33 @@
+"""
+Use case of RANGE :
+
+- We use range to know the difference between max and min value [max - min]
+
+Example :
+- Suppose there is a data of temparature,
+temp = [22, 22, 25, 34, 43, 21]
+
+"""
+
+temparature = [22, 22, 25, 34, 43, 21, 45]
+
+max_temp = max(temparature)
+min_temp = min(temparature)
+
+print("Maximum temparature : ",max_temp)
+print("Minimum temparature : ", min_temp)
+
+# difference between max and min temp
+
+difference = max_temp - min_temp
+print("\nDifference between max temparature and min temparature : ", difference, "\n")
+
+# now identifying the difference between all temp with max temp
+
+for i in range(0, len(temparature)):
+
+    if temparature[i] == max_temp:
+        continue
+    print(f"Day {i+1} : {temparature[i]}")
+    diff = max_temp - temparature[i]
+    print(f"Differnce between Day {i+1} {temparature[i]} with Day {temparature.index(max_temp)+1} {max_temp} : ", diff)
--- Descritive Stats/Measure_Of_Dispersion/concept.md
+++ Descritive Stats/Measure_Of_Dispersion/concept.md
@@ -3,6 +3,7 @@
 1. Measure of dispersion helps to measure the spread of each data in a dataset.<br>
 2. It helps to identify how much each data is spread from the mean or average.<br>
 3. Measure of dispersion helps to identify the distribution of the data.
+4. How much the data is scattered from the middle 
 </h5>
 
 ## Types
