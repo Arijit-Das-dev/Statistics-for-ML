df48b45ace9861ad26e8d5e13be3ee788ed0ae21
Arijit Das <arijitdas.das99@gmail.com>
2026-07-30 12:03:03 +0530

    Mean

--- Descritive Stats/Mean(Average)/concept1.py
+++ Descritive Stats/Mean(Average)/concept1.py
@@ -1,1 +1,21 @@
-import numpy as np
+# Mean
+# Sample mean
+# Population mean
+
+# Population mean
+population = [12, 13, 22, 24, 34, 15]
+
+xi = sum(population)
+N = len(population)
+
+U = xi/N
+print("Population mean : ", U)
+
+# Sample mean
+sample = [12, 13, 24]
+
+xi = sum(sample)
+n = len(sample)
+
+X = xi/n
+print("Sample mean : ", X)
